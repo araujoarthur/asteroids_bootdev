5ce52d00d2b7f9c2a872f1f6af7e84136011b06b
Arthur Galdino <28057772+araujoarthur@users.noreply.github.com>
2025-02-14 00:56:30 -0300

    created circle shapes

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
    #print(f"Screen height: {SCREEN_HEIGHT}")
 
    pygame.init()
+   clock = pygame.time.Clock()
+   dt = 0
    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
    while True:
@@ -16,6 +18,9 @@ def main():
          if event.type == pygame.QUIT:
             return
       
+      # 
+      dt = clock.tick(60)/1000
+      print(dt)
       pygame.display.flip()
 
 if __name__ == "__main__":
